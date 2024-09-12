e630ec5e346ca159f738f5679d24a1fcfc3720d0
wtwo1529 <003axelw@gmail.com>
2024-09-12 22:58:37 +0800

    distribution of first error handling commit

--- src/datetime.js
+++
@@ -1,9 +0,0 @@
-export default function getDate(dateString) {
-  let hours = dateString.substring(0, 2);
-  let minutes = dateString.substring(3, 5);
-  let seconds = dateString.substring(6, 8);
-  let dateObject = new Date();
-  dateObject.setHours(hours, minutes, seconds);
-
-  return dateObject;
-}
